8d1d7838a00988a98d41ee0d2dacac4b47876211
Sudharsshan S Y <sudharsshansy@gmail.com>
2025-07-24 11:20:34 +0530

    removed unwanted functions

--- lib/pages/lighthouse/animated_home.dart
+++ lib/pages/lighthouse/animated_home.dart
@@ -1,7 +1,4 @@
-import 'dart:async';
-import 'dart:math';
 import 'package:flutter/material.dart';
-import 'target_page.dart';
 
 class AnimatedHome extends StatefulWidget {
   const AnimatedHome({super.key});
@@ -11,84 +8,10 @@ class AnimatedHome extends StatefulWidget {
 }
 
 class _AnimatedHomeState extends State<AnimatedHome> {
-  final Random _random = Random();
-  final List<Offset> _positions = List.generate(3, (_) => Offset.zero);
-  Timer? _timer;
 
   @override
   void initState() {
     super.initState();
-    _randomizePositions();
-    _startFloating();
-  }
-
-  void _startFloating() {
-    _timer = Timer.periodic(const Duration(seconds: 4), (_) {
-      _randomizePositions();
-    });
-  }
-
-  void _randomizePositions() {
-    setState(() {
-      for (int i = 0; i < _positions.length; i++) {
-        _positions[i] = Offset(
-          50.0 + _random.nextDouble() * 250,
-          100.0 + _random.nextDouble() * 400,
-        );
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    _timer?.cancel();
-    super.dispose();
-  }
-
-  void _navigateToPage(int index) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (_) => TargetPage(index: index),
-      ),
-    );
-  }
-
-  Widget _buildFloatingButton(int index, Color color, String label) {
-    return AnimatedPositioned(
-      duration: const Duration(seconds: 4),
-      curve: Curves.easeInOut,
-      left: _positions[index].dx,
-      top: _positions[index].dy,
-      child: GestureDetector(
-        onTap: () => _navigateToPage(index),
-        child: Container(
-          width: 70,
-          height: 70,
-          decoration: BoxDecoration(
-            color: color.withAlpha(80),
-            border: Border.all(color: color, width: 2),
-            shape: BoxShape.circle,
-            boxShadow: [
-              BoxShadow(
-                color: color.withAlpha(122),
-                blurRadius: 12,
-                spreadRadius: 2,
-              )
-            ],
-          ),
-          child: Center(
-            child: Text(label, style: TextStyle(shadows: [
-              Shadow(
-                color: Colors.black.withAlpha(122),
-                offset: const Offset(1, 1),
-                blurRadius: 2)
-              ]),
-            ),
-          ),
-        ),
-      ),
-    );
   }
 
   @override
